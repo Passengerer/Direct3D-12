31d17336eb72ba6ed1948ca0f35536389b2bcb84
Passenger <Passenger@world.com>
2019-04-21 16:59:18 +0800

    第一章

---
+++ d3d12/Chapter1/Chapter1-3/Chapter1-3/main.cpp
@@ -0,0 +1,30 @@
+#include <Windows.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <iostream>
+using namespace std;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+int main()
+{
+	cout.precision(8);
+	if (!XMVerifyCPUSupport) {
+		cout << "directx math not supported" << endl;
+		return 0;
+	}
+	XMVECTOR u = XMVectorSet(1.0f, 1.0f, 1.0f, 0.0f);
+	XMVECTOR n = XMVector3Normalize(u);
+
+	float LU = XMVectorGetX(XMVector3Length(n));
+	cout << LU << endl;
+	if (LU == 1.0f)
+		cout << "Length 1" << endl;
+	else cout << "Length not 1" << endl;
+
+	float powLU = powf(LU, 1.0e6f);
+	cout << "LU^(10^6) = " << powLU << endl;
+
+	system("pause");
+	return 0;
+}
